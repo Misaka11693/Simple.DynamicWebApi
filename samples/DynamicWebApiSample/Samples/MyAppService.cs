74c07e8434982e61255c9f42aacc59281f4d19f3
Misaka11639 <1647988592@qq.com>
2025-06-30 18:45:12 +0800

    将版本号从 `1.0.3` 更新为 `1.0.4`。

---
+++ samples/DynamicWebApiSample/Samples/MyAppService.cs
@@ -0,0 +1,11 @@
+﻿using Simple.DynamicWebApi;
+
+namespace WebApplication1;
+
+public class MyAppService : IDynamicWebApi
+{
+    public Task<string> Get()
+    {
+        return Task.FromResult("Hello, world!");
+    }
+}
